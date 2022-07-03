d5fdc7e4158434a0a3b2ab784a30dce1553de789
venom9954 <surzhikov99@gmail.com>
2022-07-03 16:10:30 +0300

    Немного изменил логику программы. Проинициализировал аргументы и переменные. Повесил Listener-ы на кнопки. Расписал логику работы калькулятора

--- app/src/main/java/com/example/homeworkthree/MainActivity.java
+++ app/src/main/java/com/example/homeworkthree/MainActivity.java
@@ -8,11 +8,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView text_output;
-    private int firstArg;
-    private int secondArg;
+    private TextView text_output;
+    private String firstArg;
+    private String secondArg;
+    Double result;
+    private String operator;
 
-    StringBuilder inputStr = new StringBuilder();
+
+    private  StringBuilder inputStr = new StringBuilder();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,75 +35,140 @@ public class MainActivity extends AppCompatActivity {
                 R.id.button_6,
                 R.id.button_7,
                 R.id.button_8,
-                R.id.button_9
+                R.id.button_9,
+                R.id.button_point,
+                R.id.button_plusOrMinus
         };
 
         int[] otherId = new int[]{
                 R.id.button_plus,
                 R.id.button_minus,
                 R.id.button_multiply,
-                R.id.button_divide,
-                R.id.button_point,
-                R.id.button_equals
+                R.id.button_divide
         };
 
+
+        View.OnClickListener ButtonClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button_7:
+                        inputStr.append("7");
+                        break;
+                    case R.id.button_8:
+                        inputStr.append("8");
+                        break;
+                    case R.id.button_9:
+                        inputStr.append("9");
+                        break;
+                    case R.id.button_4:
+                        inputStr.append("4");
+                        break;
+                    case R.id.button_5:
+                        inputStr.append("5");
+                        break;
+                    case R.id.button_6:
+                        inputStr.append("6");
+                        break;
+                    case R.id.button_1:
+                        inputStr.append("1");
+                        break;
+                    case R.id.button_2:
+                        inputStr.append("2");
+                        break;
+                    case R.id.button_3:
+                        inputStr.append("3");
+                        break;
+                    case R.id.button_0:
+                        inputStr.append("0");
+                        break;
+                    case R.id.button_point:
+                        inputStr.append(".");
+                        break;
+                    case R.id.button_plusOrMinus:
+                        inputStr.append("-");
+                        break;
+
+
+                }
+                text_output.setText(inputStr);
+
+
+            }
+        };
+        View.OnClickListener operationButton = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstArg = text_output.getText().toString();
+                switch (view.getId()) {
+                    case R.id.button_divide:
+                        operator = "/";
+                        inputStr.setLength(0);
+                        break;
+                    case R.id.button_minus:
+                        operator = "-";
+                        inputStr.setLength(0);
+                        break;
+                    case R.id.button_plus:
+                        operator = "+";
+                        inputStr.setLength(0);
+                        break;
+                    case R.id.button_multiply:
+                        operator = "*";
+                        inputStr.setLength(0);
+                        break;
+
+
+
+                }
+                text_output.setText(operator);
+            }
+        };
+
+
         for (int i = 0; i < numbersId.length; i++) {
-            findViewById(numbersId[i]).setOnClickListener(listener);
+            findViewById(numbersId[i]).setOnClickListener(ButtonClickListener);
         }
 
         for (int i = 0; i < otherId.length; i++) {
-            findViewById(otherId[i]).setOnClickListener(listener);
+            findViewById(otherId[i]).setOnClickListener(operationButton);
         }
 
-    }
 
+        findViewById(R.id.button_equals).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                secondArg = text_output.getText().toString();
 
-    View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            onNumPressed(view.getId());
-            text_output.setText(inputStr.toString());
-        }
-    };
-
-    public void onNumPressed(int buttonsId) {
-
-
-        if (inputStr.length() < 9) {
-            switch (buttonsId) {
-                case R.id.button_0:
-
-                    inputStr.append("0");
-
-                    break;
-                case R.id.button_1:
-                    inputStr.append("1");
-                    break;
-                case R.id.button_2:
-                    inputStr.append("2");
-                    break;
-                case R.id.button_3:
-                    inputStr.append("3");
-                    break;
-                case R.id.button_4:
-                    inputStr.append("4");
-                    break;
-                case R.id.button_5:
-                    inputStr.append("5");
-                    break;
-                case R.id.button_6:
-                    inputStr.append("6");
-                    break;
-                case R.id.button_7:
-                    inputStr.append("7");
-                    break;
-                case R.id.button_8:
-                    inputStr.append("8");
-                    break;
-                case R.id.button_9:
-                    inputStr.append("9");
-                    break;
+                if(operator == "/"){
+                    result = Double.parseDouble(firstArg) / Double.parseDouble(secondArg);
+                }
+                else if(operator == "-") {
+                    result = Double.parseDouble(firstArg) - Double.parseDouble(secondArg);
+                }
+                else if(operator == "+") {
+                    result = Double.parseDouble(firstArg) + Double.parseDouble(secondArg);
+                }
+                else if (operator == "*") {
+                    result = Double.parseDouble(firstArg) * Double.parseDouble(secondArg);
+                }
+                text_output.setText(result.toString());
+                inputStr.setLength(0);
             }
-        }
+
+        });
+
+        findViewById(R.id.button_AC).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text_output.setText("");
+                inputStr.setLength(0);
+            }
+
+        });
+
     }
+
+
 }
