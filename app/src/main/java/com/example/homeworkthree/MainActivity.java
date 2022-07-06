2c326b153ec29d89618ea2aba1b80316f7a1c156
venom9954 <surzhikov99@gmail.com>
2022-07-06 12:44:29 +0300

    Кнопки переделаны в материал дизайн

--- app/src/main/java/com/example/homeworkthree/MainActivity.java
+++ app/src/main/java/com/example/homeworkthree/MainActivity.java
@@ -14,6 +14,28 @@ public class MainActivity extends AppCompatActivity {
     private Double result;
     private String operator;
 
+    int[] numbersId = new int[]{
+            R.id.button_0,
+            R.id.button_1,
+            R.id.button_2,
+            R.id.button_3,
+            R.id.button_4,
+            R.id.button_5,
+            R.id.button_6,
+            R.id.button_7,
+            R.id.button_8,
+            R.id.button_9,
+            R.id.button_point,
+            R.id.button_plusOrMinus
+    };
+
+    int[] otherId = new int[]{
+            R.id.button_plus,
+            R.id.button_minus,
+            R.id.button_multiply,
+            R.id.button_divide
+    };
+
 
     private StringBuilder inputStr = new StringBuilder();
 
@@ -25,114 +47,94 @@ public class MainActivity extends AppCompatActivity {
 
         text_output = findViewById(R.id.text_output);
 
-        int[] numbersId = new int[]{
-                R.id.button_0,
-                R.id.button_1,
-                R.id.button_2,
-                R.id.button_3,
-                R.id.button_4,
-                R.id.button_5,
-                R.id.button_6,
-                R.id.button_7,
-                R.id.button_8,
-                R.id.button_9,
-                R.id.button_point,
-                R.id.button_plusOrMinus
-        };
-
-        int[] otherId = new int[]{
-                R.id.button_plus,
-                R.id.button_minus,
-                R.id.button_multiply,
-                R.id.button_divide
-        };
-
 
         View.OnClickListener ButtonClickListener = new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.button_7:
-                        inputStr.append("7");
-                        break;
-                    case R.id.button_8:
-                        inputStr.append("8");
-                        break;
-                    case R.id.button_9:
-                        inputStr.append("9");
-                        break;
-                    case R.id.button_4:
-                        inputStr.append("4");
-                        break;
-                    case R.id.button_5:
-                        inputStr.append("5");
-                        break;
-                    case R.id.button_6:
-                        inputStr.append("6");
-                        break;
-                    case R.id.button_1:
-                        inputStr.append("1");
-                        break;
-                    case R.id.button_2:
-                        inputStr.append("2");
-                        break;
-                    case R.id.button_3:
-                        inputStr.append("3");
-                        break;
-                    case R.id.button_0:
-                        inputStr.append("0");
-                        break;
-                    case R.id.button_point:
-                        if (dotIsPresent(inputStr)) {
 
-                        } else {
-                            inputStr.append(".");
+                for (int i=0; i<numbersId.length; i++) {
+                    if (view.getId() == numbersId[i]) {
+                        switch (view.getId()) {
+                            case R.id.button_7:
+                                inputStr.append("7");
+                                break;
+                            case R.id.button_8:
+                                inputStr.append("8");
+                                break;
+                            case R.id.button_9:
+                                inputStr.append("9");
+                                break;
+                            case R.id.button_4:
+                                inputStr.append("4");
+                                break;
+                            case R.id.button_5:
+                                inputStr.append("5");
+                                break;
+                            case R.id.button_6:
+                                inputStr.append("6");
+                                break;
+                            case R.id.button_1:
+                                inputStr.append("1");
+                                break;
+                            case R.id.button_2:
+                                inputStr.append("2");
+                                break;
+                            case R.id.button_3:
+                                inputStr.append("3");
+                                break;
+                            case R.id.button_0:
+                                inputStr.append("0");
+                                break;
+                            case R.id.button_point:
+                                if (dotIsPresent(inputStr)) {
+                                } else {
+                                    inputStr.append(".");
+                                }
+                                break;
+                            case R.id.button_plusOrMinus:
+                                if (minusIsPresent(inputStr)) {
+                                } else {
+                                    inputStr.append("-");
+                                }
+                                break;
                         }
+                        text_output.setText(inputStr);
 
-                        break;
-                    case R.id.button_plusOrMinus:
-                        if(minusIsPresent(inputStr)) {
-                        } else {
-                        inputStr.append("-");
                     }
 
-                    break;
-
 
                 }
-                text_output.setText(inputStr);
+                    firstArg = inputStr.toString();
 
+                    for(int i=0; i<otherId.length; i++){
+                        if (view.getId() == otherId[i]){
+                            switch (view.getId()) {
+                                case R.id.button_divide:
+                                    operator = "/";
+                                    inputStr.setLength(0);
+                                    break;
+                                case R.id.button_minus:
+                                    operator = "-";
+                                    inputStr.setLength(0);
+                                    break;
+                                case R.id.button_plus:
+                                    operator = "+";
+                                    inputStr.setLength(0);
+                                    break;
+                                case R.id.button_multiply:
+                                    operator = "*";
+                                    inputStr.setLength(0);
+                                    break;
+                            }
+                            text_output.setText(operator);
+                        }
+
+                }
 
             }
         };
-        View.OnClickListener operationButton = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                firstArg = text_output.getText().toString();
-                switch (view.getId()) {
-                    case R.id.button_divide:
-                        operator = "/";
-                        inputStr.setLength(0);
-                        break;
-                    case R.id.button_minus:
-                        operator = "-";
-                        inputStr.setLength(0);
-                        break;
-                    case R.id.button_plus:
-                        operator = "+";
-                        inputStr.setLength(0);
-                        break;
-                    case R.id.button_multiply:
-                        operator = "*";
-                        inputStr.setLength(0);
-                        break;
 
-
-                }
-                text_output.setText(operator);
-            }
-        };
 
 
         for (int i = 0; i < numbersId.length; i++) {
@@ -140,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         for (int i = 0; i < otherId.length; i++) {
-            findViewById(otherId[i]).setOnClickListener(operationButton);
+            findViewById(otherId[i]).setOnClickListener(ButtonClickListener);
         }
 
 
@@ -170,10 +172,7 @@ public class MainActivity extends AppCompatActivity {
                 text_output.setText("");
                 inputStr.setLength(0);
             }
-
         });
-
-
     }
 
     public boolean dotIsPresent(StringBuilder inputStr) {
